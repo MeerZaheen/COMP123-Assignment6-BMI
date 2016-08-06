07963f593702447977469407d33d3be071887ea3
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-08-06 02:48:27 -0400

    Final Commit

--- comp123-assignment6-BMI/MainForm.Designer.cs
+++ comp123-assignment6-BMI/MainForm.Designer.cs
@@ -44,6 +44,7 @@
             this.OverweightLabel = new System.Windows.Forms.Label();
             this.NormalLabel = new System.Windows.Forms.Label();
             this.UnderwightLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.Units.SuspendLayout();
             this.BMIGroupBox.SuspendLayout();
             this.SuspendLayout();
@@ -53,11 +54,9 @@
             this.Units.Controls.Add(this.ImperialRadioButton);
             this.Units.Controls.Add(this.MetricRadioButton);
             this.Units.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Units.Location = new System.Drawing.Point(61, 40);
-            this.Units.Margin = new System.Windows.Forms.Padding(2);
+            this.Units.Location = new System.Drawing.Point(96, 65);
             this.Units.Name = "Units";
-            this.Units.Padding = new System.Windows.Forms.Padding(2);
-            this.Units.Size = new System.Drawing.Size(167, 59);
+            this.Units.Size = new System.Drawing.Size(256, 142);
             this.Units.TabIndex = 2;
             this.Units.TabStop = false;
             this.Units.Text = "Units";
@@ -65,77 +64,70 @@
             // ImperialRadioButton
             // 
             this.ImperialRadioButton.AutoSize = true;
-            this.ImperialRadioButton.Location = new System.Drawing.Point(80, 23);
-            this.ImperialRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ImperialRadioButton.Location = new System.Drawing.Point(15, 87);
             this.ImperialRadioButton.Name = "ImperialRadioButton";
-            this.ImperialRadioButton.Size = new System.Drawing.Size(83, 24);
+            this.ImperialRadioButton.Size = new System.Drawing.Size(230, 33);
             this.ImperialRadioButton.TabIndex = 4;
-            this.ImperialRadioButton.Text = "Imperial";
+            this.ImperialRadioButton.Text = "Imperial (inch/lbs)";
             this.ImperialRadioButton.UseVisualStyleBackColor = true;
             // 
             // MetricRadioButton
             // 
             this.MetricRadioButton.AutoSize = true;
             this.MetricRadioButton.Checked = true;
-            this.MetricRadioButton.Location = new System.Drawing.Point(10, 23);
-            this.MetricRadioButton.Margin = new System.Windows.Forms.Padding(2);
+            this.MetricRadioButton.Location = new System.Drawing.Point(15, 35);
             this.MetricRadioButton.Name = "MetricRadioButton";
-            this.MetricRadioButton.Size = new System.Drawing.Size(70, 24);
+            this.MetricRadioButton.Size = new System.Drawing.Size(179, 33);
             this.MetricRadioButton.TabIndex = 3;
             this.MetricRadioButton.TabStop = true;
-            this.MetricRadioButton.Text = "Metric";
+            this.MetricRadioButton.Text = "Metric (m/kg)";
             this.MetricRadioButton.UseVisualStyleBackColor = true;
             // 
             // HeightTextBox
             // 
             this.HeightTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.HeightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HeightTextBox.Location = new System.Drawing.Point(121, 161);
-            this.HeightTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.HeightTextBox.Location = new System.Drawing.Point(138, 248);
             this.HeightTextBox.Name = "HeightTextBox";
-            this.HeightTextBox.Size = new System.Drawing.Size(143, 19);
+            this.HeightTextBox.Size = new System.Drawing.Size(214, 28);
             this.HeightTextBox.TabIndex = 5;
             // 
             // HeightLabel
             // 
             this.HeightLabel.AutoSize = true;
             this.HeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HeightLabel.Location = new System.Drawing.Point(23, 161);
-            this.HeightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.HeightLabel.Location = new System.Drawing.Point(34, 248);
             this.HeightLabel.Name = "HeightLabel";
-            this.HeightLabel.Size = new System.Drawing.Size(87, 20);
+            this.HeightLabel.Size = new System.Drawing.Size(95, 29);
             this.HeightLabel.TabIndex = 4;
-            this.HeightLabel.Text = "Height (m):";
+            this.HeightLabel.Text = "Height :";
             // 
             // WeightLabel
             // 
             this.WeightLabel.AutoSize = true;
             this.WeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WeightLabel.Location = new System.Drawing.Point(23, 187);
-            this.WeightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.WeightLabel.Location = new System.Drawing.Point(34, 288);
             this.WeightLabel.Name = "WeightLabel";
-            this.WeightLabel.Size = new System.Drawing.Size(94, 20);
+            this.WeightLabel.Size = new System.Drawing.Size(100, 29);
             this.WeightLabel.TabIndex = 6;
-            this.WeightLabel.Text = "Weight (kg):";
+            this.WeightLabel.Text = "Weight :";
             // 
             // WeightTextBox
             // 
             this.WeightTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.WeightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WeightTextBox.Location = new System.Drawing.Point(121, 187);
-            this.WeightTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.WeightTextBox.Location = new System.Drawing.Point(138, 288);
             this.WeightTextBox.Name = "WeightTextBox";
-            this.WeightTextBox.Size = new System.Drawing.Size(143, 19);
+            this.WeightTextBox.Size = new System.Drawing.Size(214, 28);
             this.WeightTextBox.TabIndex = 6;
             // 
             // BMILabel
             // 
             this.BMILabel.AutoSize = true;
             this.BMILabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BMILabel.Location = new System.Drawing.Point(23, 246);
-            this.BMILabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.BMILabel.Location = new System.Drawing.Point(34, 378);
             this.BMILabel.Name = "BMILabel";
-            this.BMILabel.Size = new System.Drawing.Size(42, 20);
+            this.BMILabel.Size = new System.Drawing.Size(61, 29);
             this.BMILabel.TabIndex = 8;
             this.BMILabel.Text = "BMI:";
             // 
@@ -145,19 +137,17 @@
             this.BMITextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.BMITextBox.Enabled = false;
             this.BMITextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BMITextBox.Location = new System.Drawing.Point(121, 246);
-            this.BMITextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.BMITextBox.Location = new System.Drawing.Point(138, 378);
             this.BMITextBox.Name = "BMITextBox";
-            this.BMITextBox.Size = new System.Drawing.Size(143, 19);
+            this.BMITextBox.Size = new System.Drawing.Size(214, 28);
             this.BMITextBox.TabIndex = 8;
             // 
             // CalculateButton
             // 
             this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CalculateButton.Location = new System.Drawing.Point(27, 418);
-            this.CalculateButton.Margin = new System.Windows.Forms.Padding(2);
+            this.CalculateButton.Location = new System.Drawing.Point(40, 643);
             this.CalculateButton.Name = "CalculateButton";
-            this.CalculateButton.Size = new System.Drawing.Size(86, 26);
+            this.CalculateButton.Size = new System.Drawing.Size(129, 40);
             this.CalculateButton.TabIndex = 10;
             this.CalculateButton.Text = "Calculate";
             this.CalculateButton.UseVisualStyleBackColor = true;
@@ -166,10 +156,9 @@
             // ClearButton
             // 
             this.ClearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClearButton.Location = new System.Drawing.Point(179, 418);
-            this.ClearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ClearButton.Location = new System.Drawing.Point(268, 643);
             this.ClearButton.Name = "ClearButton";
-            this.ClearButton.Size = new System.Drawing.Size(85, 26);
+            this.ClearButton.Size = new System.Drawing.Size(128, 40);
             this.ClearButton.TabIndex = 11;
             this.ClearButton.Text = "Clear";
             this.ClearButton.UseVisualStyleBackColor = true;
@@ -182,11 +171,9 @@
             this.BMIGroupBox.Controls.Add(this.NormalLabel);
             this.BMIGroupBox.Controls.Add(this.UnderwightLabel);
             this.BMIGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BMIGroupBox.Location = new System.Drawing.Point(85, 296);
-            this.BMIGroupBox.Margin = new System.Windows.Forms.Padding(2);
+            this.BMIGroupBox.Location = new System.Drawing.Point(128, 455);
             this.BMIGroupBox.Name = "BMIGroupBox";
-            this.BMIGroupBox.Padding = new System.Windows.Forms.Padding(2);
-            this.BMIGroupBox.Size = new System.Drawing.Size(133, 96);
+            this.BMIGroupBox.Size = new System.Drawing.Size(200, 148);
             this.BMIGroupBox.TabIndex = 16;
             this.BMIGroupBox.TabStop = false;
             this.BMIGroupBox.Text = "BMI";
@@ -195,10 +182,9 @@
             // 
             this.ObesityLabel.AutoSize = true;
             this.ObesityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ObesityLabel.Location = new System.Drawing.Point(4, 73);
-            this.ObesityLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ObesityLabel.Location = new System.Drawing.Point(6, 112);
             this.ObesityLabel.Name = "ObesityLabel";
-            this.ObesityLabel.Size = new System.Drawing.Size(75, 15);
+            this.ObesityLabel.Size = new System.Drawing.Size(113, 22);
             this.ObesityLabel.TabIndex = 3;
             this.ObesityLabel.Text = "Obesity>=30";
             // 
@@ -206,10 +192,9 @@
             // 
             this.OverweightLabel.AutoSize = true;
             this.OverweightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.OverweightLabel.Location = new System.Drawing.Point(4, 55);
-            this.OverweightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.OverweightLabel.Location = new System.Drawing.Point(6, 85);
             this.OverweightLabel.Name = "OverweightLabel";
-            this.OverweightLabel.Size = new System.Drawing.Size(129, 15);
+            this.OverweightLabel.Size = new System.Drawing.Size(193, 22);
             this.OverweightLabel.TabIndex = 2;
             this.OverweightLabel.Text = "Overweight = 25~29.9 ";
             // 
@@ -217,10 +202,9 @@
             // 
             this.NormalLabel.AutoSize = true;
             this.NormalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NormalLabel.Location = new System.Drawing.Point(4, 36);
-            this.NormalLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.NormalLabel.Location = new System.Drawing.Point(6, 55);
             this.NormalLabel.Name = "NormalLabel";
-            this.NormalLabel.Size = new System.Drawing.Size(119, 15);
+            this.NormalLabel.Size = new System.Drawing.Size(174, 22);
             this.NormalLabel.TabIndex = 1;
             this.NormalLabel.Text = "Normal = 18.5~24.9 ";
             // 
@@ -228,18 +212,26 @@
             // 
             this.UnderwightLabel.AutoSize = true;
             this.UnderwightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UnderwightLabel.Location = new System.Drawing.Point(4, 17);
-            this.UnderwightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.UnderwightLabel.Location = new System.Drawing.Point(6, 26);
             this.UnderwightLabel.Name = "UnderwightLabel";
-            this.UnderwightLabel.Size = new System.Drawing.Size(121, 15);
+            this.UnderwightLabel.Size = new System.Drawing.Size(178, 22);
             this.UnderwightLabel.TabIndex = 0;
             this.UnderwightLabel.Text = "Underweight = <18.5";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(138, 419);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(214, 30);
+            this.progressBar1.TabIndex = 17;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
             // BodyMassIndexForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 452);
+            this.ClientSize = new System.Drawing.Size(438, 697);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.BMIGroupBox);
             this.Controls.Add(this.ClearButton);
             this.Controls.Add(this.CalculateButton);
@@ -250,7 +242,6 @@
             this.Controls.Add(this.HeightLabel);
             this.Controls.Add(this.HeightTextBox);
             this.Controls.Add(this.Units);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "BodyMassIndexForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Body Mass Index";
@@ -282,5 +273,6 @@
         private System.Windows.Forms.Label NormalLabel;
         private System.Windows.Forms.Label OverweightLabel;
         private System.Windows.Forms.Label ObesityLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- comp123-assignment6-BMI/MainForm.cs
+++ comp123-assignment6-BMI/MainForm.cs
@@ -7,9 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/**
+ * Author : Meer Zaheen Nazmul
+ * Student # : 300522487
+ * Date : August, 5th, 2016
+ * Description : Main program for BMI Calculator App
+ * Version : 0.0.5 : Final Commit - Updated UI
+ */
 namespace BodyMassIndex
 {
+    /**
+    * <summary>
+    * This class is the "driver" class for our program
+    * </summary>
+    * 
+    * @class Program
+    */
     public partial class BodyMassIndexForm : Form
     {
 
@@ -17,6 +30,15 @@ namespace BodyMassIndex
         {
             InitializeComponent();
         }
+        /**
+         * <summary>
+         * This eventhandler calculate the BMI and display on the textboxes
+         * </summary>
+         * 
+         * @method CalculateButton_Click
+         * @param {object} sender
+         * @param {EventArgs} e
+         */
 
         //Calculate Button+++++++++++++++++++++++++++++++++++++++++++++
         private void CalculateButton_Click(object sender, EventArgs e)
@@ -29,6 +51,7 @@ namespace BodyMassIndex
                 //convert to numbers
                 height = Convert.ToDouble(HeightTextBox.Text);
                 weight = Convert.ToDouble(WeightTextBox.Text);
+
 
                 //each textbox can't be empty
                 if (HeightTextBox.Text == "" || WeightTextBox.Text == "")
@@ -53,27 +76,41 @@ namespace BodyMassIndex
                     //calculates the BMI Metric
                     if (MetricRadioButton.Checked == true)
                     {
+                        
                         //algorithm
                         bMI = weight / ((height) * (height));
                         BMITextBox.Text = Convert.ToString(bMI);
 
+                        // condition to set progress bar value
+                        if (((bMI / 30) * 100) > 100)
+                        {
+                            progressBar1.Value = 100;
+                        }
+                        else
+                        {
+                            progressBar1.Value = ((int)((bMI / 30) * 100));
+                        }
 
-                        //detarmine BMI
+                        //determine BMI
                         if (bMI < 18.5)
                         {
                             MessageBox.Show("Underweight < 18.5");
+                            progressBar1.ForeColor = Color.Yellow;
                         }
                         else if (bMI >= 18.5 && bMI <= 24.9)
                         {
                             MessageBox.Show("Normal weight = 18.5~24.9 ");
+                            progressBar1.ForeColor = Color.Green;
                         }
                         else if (bMI > 25 && bMI <= 29.9)
                         {
                             MessageBox.Show("Overweight = 25~29.9");
+                            progressBar1.ForeColor = Color.DarkGreen;
                         }
                         else if (bMI >= 30)
                         {
                             MessageBox.Show("Obesity >= 30");
+                            progressBar1.ForeColor = Color.Red;
                         }
 
                     }
@@ -84,22 +121,36 @@ namespace BodyMassIndex
                         bMI = (weight * 4.88) / ((height) * (height));
                         BMITextBox.Text = Convert.ToString(bMI);
 
+                        // condition to set progress bar value
+                        if (((bMI / 30) * 100) > 100)
+                        {
+                            progressBar1.Value = 100;
+                        }
+                        else
+                        {
+                            progressBar1.Value = ((int)((bMI / 30) * 100));
+                        }
+
                         //detarmine BMI
                         if (bMI < 18.5)
                         {
                             MessageBox.Show("Underweight < 18.5");
+                            progressBar1.ForeColor = Color.Yellow;
                         }
                         else if (bMI >= 18.5 && bMI <= 24.9)
                         {
                             MessageBox.Show("Normal weight = 18.5~24.9 ");
+                            progressBar1.ForeColor = Color.Green;
                         }
                         else if (bMI > 25 && bMI <= 29.9)
                         {
                             MessageBox.Show("Overweight = 25~29.9");
+                            progressBar1.ForeColor = Color.DarkGreen;
                         }
                         else if (bMI >= 30)
                         {
                             MessageBox.Show("Obesity >= 30");
+                            progressBar1.ForeColor = Color.Red;
                         }
 
                     }
@@ -112,7 +163,15 @@ namespace BodyMassIndex
                 HeightTextBox.SelectAll();
             }
         }
-
+        /**
+        * <summary>
+        * This eventhandler clears the content of the form when clicked
+        * </summary>
+        * 
+        * @method ClearButton_Click
+        * @param {object} sender
+        * @param {EventArgs} e
+        */
         private void ClearButton_Click(object sender, EventArgs e)
         {
             //clear all
@@ -121,14 +180,55 @@ namespace BodyMassIndex
 
             BMITextBox.Text = "";
 
+            progressBar1.Value = 0;
+
 
             //enable all the textbox
             HeightTextBox.Enabled = true;
             WeightTextBox.Enabled = true;
 
         }
+        /**
+         * <summary>
+         * This key press eventhandler set a restriction on input height value
+         * </summary>
+         * 
+         * @method HeightTextBox_KeyPress
+         * @param {object} sender
+         * @param {KeyPressEventArgs} e
+         */
+        private void WeightTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+        /**
+         * <summary>
+         * This key press eventhandler set a restriction on input height value
+         * </summary>
+         * 
+         * @method HeightTextBox_KeyPress
+         * @param {object} sender
+         * @param {KeyPressEventArgs} e
+         */
+        private void HeightTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
 
         private void BodyMassIndexForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
         {
 
         }
